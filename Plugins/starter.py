2e5a11b3de0cba3c2cde38f73a58374d791ea3b4
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-11-02 10:30:08 +0530

    Update starter.py

--- Plugins/starter.py
+++ Plugins/starter.py
@@ -4,7 +4,7 @@ from telethon import events
 
 class start():
 
-    @bot.on(events.NewMessage(pattern=r"^/start$|^/start@Anime_Gallery_Robot"))
+    @bot.on(events.NewMessage(pattern=r"^/start$|^/start@AcuteRobot"))
     async def event_handler_start(event):
         await bot.send_message(
             event.chat_id,
@@ -12,7 +12,7 @@ class start():
             file='https://tenor.com/view/chika-fujiwara-kaguya-sama-love-is-war-anime-wink-smile-gif-18043249'
         )
 
-    @bot.on(events.NewMessage(pattern=r"^/help$|^/help@Anime_Gallery_Robot"))
+    @bot.on(events.NewMessage(pattern=r"^/help$|^/help@AcuteRobot"))
     async def event_handler_help(event):
         await bot.send_message(
             event.chat_id,
