e1ec2a81d59c7f4d6a88901832ae9de654d67ae6
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-11-02 11:22:05 +0530

    Update helper.py

--- Helper/helper.py
+++ Helper/helper.py
@@ -10,7 +10,8 @@ help_text = '''List of commands:
 /batch <animeid>:<from episode>:<to episode> : For getting download links in bulk
 
 /download : same as batch download but you get 1 file you can import in **1DM** (here the highest possible resolution is provided so if you want to choose resolution use old batch command
-                
+
+/about  : For more information about me             
                 
 The links provided are in multiple qualities to download just open links in chrome and download starts automatically
                 
