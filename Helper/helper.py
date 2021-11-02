9338e16337689ee2d3cb256070f7dcf020ef7bcd
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-11-02 11:29:19 +0530

    Update helper.py

--- Helper/helper.py
+++ Helper/helper.py
@@ -24,9 +24,9 @@ I suggest open the links in **1DM** app on playstore instead of chrome for easy 
 To report any Problems, Bugs, Suggestions go to @EDIT_REPO'''
 
 about_text='''𝙼𝚈 𝙽𝙰𝙼𝙴: 𝙻𝚄𝙽𝙰
-             𝙲𝚁𝙴𝙰𝚃𝙾𝚁: [꧁࿗༒⚔ 𝕄𝕣 ℙ𝕣𝕠𝕗𝕖𝕤𝕤𝕠𝕣⚔༒࿗꧂](https://t.meN_A_V_I_P_A_V_I/
-             𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-             𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-             𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-             𝙽𝙾𝚃𝙴 
-             𝚃𝙷𝙸𝚂 𝙸𝚂 𝙽𝙾𝚃 𝙰 𝙾𝙿𝙴𝙽 𝚂𝙾𝚄𝚁𝙲𝙴 𝙿�𝚁𝙾𝙹𝙴𝙲𝚃'''
+𝙲𝚁𝙴𝙰𝚃𝙾𝚁: [꧁࿗༒⚔ 𝕄𝕣 ℙ𝕣𝕠𝕗𝕖𝕤𝕤𝕠𝕣⚔༒࿗꧂](https://t.meN_A_V_I_P_A_V_I/
+𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
+    𝙽𝙾𝚃𝙴 
+𝚃𝙷𝙸𝚂 𝙸𝚂 𝙽𝙾𝚃 𝙰 𝙾𝙿𝙴𝙽 𝚂𝙾𝚄𝚁𝙲𝙴 𝙿�𝚁𝙾𝙹𝙴𝙲𝚃'''
