8dae7ac170c3e648f4714e0d80fe5c056074db05
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-11-02 14:30:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Plugins.starter import start
-from Plugins.anime import Anime
-from Plugins.manga import Manga
-from Plugins.nhentai import Nhentai
+from Plugins.anime import Anime.py
+from Plugins.manga import Manga.py
+from Plugins.nhentai import Nhentai.py
+from Plugins.movies import movies.py
+from Plugins.tvseries import tvseries.py
 from config import bot
+
 
 try:
     start()
