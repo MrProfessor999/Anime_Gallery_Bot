52c69e289164096667ec8b7605deef7cc8892cee
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-11-03 10:16:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from Plugins.starter import start
 from Plugins.anime import Anime
 from Plugins.manga import Manga
 from Plugins.nhentai import Nhentai
-from Plugins.movies import Movies
 from config import bot
 
 try:
@@ -10,8 +9,7 @@ try:
     Anime()
     Manga()
     Nhentai()
-    Movies()
-
+    
 except Exception as e:
     print(e)
 
