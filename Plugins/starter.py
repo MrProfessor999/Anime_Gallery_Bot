07eaf759c9b666ff4f9187f41866593ce867931f
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-11-04 21:23:57 +0530

    Update starter.py

--- Plugins/starter.py
+++ Plugins/starter.py
@@ -10,7 +10,7 @@ class start():
 
             buttons = [[
                          InlineKeyboardButton('😉ABOUT', callback_data='about')
-                         InlineKeyboardButton('HELP❗', callback_data='help'),
+                         InlineKeyboardButton('❗HELP', callback_data='help')
                      ],[ 
                          Inlinekeyboardbutton("👨‍💻DEVOLOPER", url="https://t.me/N_A_V_I_P_A_V_I")
                      ]]
