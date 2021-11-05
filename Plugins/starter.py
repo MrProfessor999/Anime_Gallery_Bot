5cfb778659202a7917d291a378b299c37bfa88f2
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-11-05 10:21:36 +0530

    Update starter.py

--- Plugins/starter.py
+++ Plugins/starter.py
@@ -6,8 +6,24 @@ from telethon import events
 class start():
 
     @bot.on(events.NewMessage(pattern=r"^/start$|^/start@Anime_Gallery_Robot"))
-    async def event_handler_start(event):
+    async def event_handler_START(event):
         await bot.send_message(
-        file='https://telegra.ph/file/de65abf86cfde85772e21.jpg'
+            event.chat_id,
+            file='https://telegra.ph/file/de65abf86cfde85772e21.jpg'
+            )
+  
+
+    @bot.on(events.NewMessage(pattern=r"^/help$|^/help@Anime_Gallery_Robot"))
+    async def event_handler_HELP(event):
+        await bot.send_message(
+            event.chat_id,
+            HELP_TXT
+            )
+
+    @bot.on(events.NewMessage(pattern="/about"))
+    async def event_handler_ABOUT(event):
+        await bot.send_message(
+            event.chat_id,
+            ABOUT_TXT
         )
-                    
+    
